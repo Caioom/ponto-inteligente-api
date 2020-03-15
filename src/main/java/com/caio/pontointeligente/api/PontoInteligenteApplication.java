40e1778b9cafedac58a14466becb544ed613ae13
caioom <caio.ds.2003@gmail.com>
2020-03-15 13:05:11 -0300

    Commit inicial dos arquivos

---
+++ src/main/java/com/caio/pontointeligente/api/PontoInteligenteApplication.java
@@ -0,0 +1,13 @@
+package com.caio.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteligenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caio/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caio.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
